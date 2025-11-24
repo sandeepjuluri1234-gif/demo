97269d6c188069d13e599f117f6e10b98b50b329
sandeepjuluri1234-gif <sandeepjuluri1234@gmail.com>
2025-11-24 11:13:57 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,19 @@
-printf("sandeep")
+
+import pyodbc
+
+conn = pyodbc.connect(
+    'DRIVER={ODBC Driver 17 for SQL Server};'
+    'SERVER=localhost;'
+    'DATABASE=TestDB;'
+    'Trusted_Connection=yes;'
+)
+
+cursor = conn.cursor()
+cursor.execute("SELECT * FROM Users")
+
+for row in cursor.fetchall():
+    print(row)
+
+conn.close()
+
+
